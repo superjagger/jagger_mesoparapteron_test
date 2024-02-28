3ad8a594b324f141a24d3146e5a8489bce12729d
superjagger <31763819+superjagger@users.noreply.github.com>
2024-02-29 05:27:37 +0800

    Add files via upload

---
+++ lib/jagger_mesoparapteron_test.rb
@@ -0,0 +1,6 @@
+require "jagger_mesoparapteron_test/version"
+
+module JaggerMesoparapteronTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_mesoparapteron_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerMesoparapteronTest
+  VERSION = "0.1.0"
+end
